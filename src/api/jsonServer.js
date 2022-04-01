3c6a9d449fcac58600d03206a22838b9fb99f68e
Smitkumar Contractor <smitkumarcontractor@Smitkumars-MacBook-Pro.local>
2022-04-01 12:47:59 -0700

    Added jsonserver

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://f4ec-12-28-27-194.ngrok.io'
+});
---
+++ src/screens/EditScreen.js
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect } from 'react';
+import { View, Text, StyleSheet, TextInput, Button, Title } from 'react-native';
+
+import { Context } from '../context/BlogContext';
+
+import BlogPostForm from '../components/BlogPostForm';
+
+const EditScreen = ({ navigation }) => {
+    const id = navigation.getParam('id');
+    const { state, editBlogPost } = useContext(Context);
+
+    const blogPost = state.find((blogPost) => blogPost.id === id);
+
+    return (
+        <BlogPostForm
+            initialValues={{ title: blogPost.title, content: blogPost.content }}
+            onSubmit={(title, content) => {
+                editBlogPost(id, title, content, () => navigation.pop());
+            }}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5,
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5,
+    }
+});
+
+export default EditScreen;
